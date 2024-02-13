870c339e2f27f930a6eadfc9c4cd96e83682afb6
Maksim Pumé <94569453+DDefo25@users.noreply.github.com>
2024-02-13 08:22:41 +0000

    add axiosBaseQuery

---
+++ frontend/src/store/axiosBaseQuery.ts
@@ -0,0 +1,37 @@
+import instance from "../config/axiosConfig"
+import type { BaseQueryFn } from '@reduxjs/toolkit/query'
+import type { AxiosError, AxiosRequestConfig } from 'axios'
+
+export const axiosBaseQuery = 
+    ({ baseUrl } : {baseUrl: string} = { baseUrl: '' })
+    : BaseQueryFn<
+        {
+        url: string
+        method?: AxiosRequestConfig['method']
+        data?: AxiosRequestConfig['data']
+        params?: AxiosRequestConfig['params']
+        headers?: AxiosRequestConfig['headers']
+        },
+        unknown,
+        unknown
+    > =>
+    async ({ url, method, data, params, headers }) => {
+        try {
+            const { data: dataResponse } = await instance({
+            url: baseUrl + url,
+            method,
+            data,
+            params,
+            headers,
+            })
+            return { data: dataResponse }
+        } catch (axiosError) {
+            const err = axiosError as AxiosError
+            return {
+            error: {
+                status: err.response?.status,
+                data: err.response?.data || err.message,
+            },
+            }
+        }
+    }
